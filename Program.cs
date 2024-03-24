267661b4af8c3e3352a0593c6f37955d43f3912a
genna <genna@GENNADIY>
2024-03-24 21:23:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        #region Work 1
+
+    //        List<int> list = new List<int>();
+    //        var rand = new Random();
+    //        for (int i = 0; i < 100; i++)
+    //        {
+    //            list.Add(rand.Next(0, 100));
+    //        }
+
+    //        for (int i = 0; i < list.Count; i++)
+    //        {
+    //            Console.WriteLine(list[i]);
+    //        }
+
+    //        Console.ReadLine();
+
+    //        for (int i = 0; i < list.Count; i++)
+    //        {
+    //            if (list[i] > 25 & list[i] < 50)
+    //            {
+    //                list.RemoveAt(i);
+    //                i--;
+    //            }
+    //        }
+
+    //        for (int i = 0; i < list.Count; i++)
+    //        {
+    //            Console.WriteLine(list[i]);
+    //        }
+    //        #endregion
+    //    }
+    //}
+
---
+++ TelephoneBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        #region Work 2
+        Dictionary<string, string> phone = new Dictionary<string, string>();
+
+        string? phoneNumber = Console.ReadLine();
+        string? fio = Console.ReadLine();
+
+        phone.Add(phoneNumber, fio);
+        
+        while(true)
+        {
+            if(phoneNumber != null ) 
+            {
+                phoneNumber = Console.ReadLine();
+                if (phoneNumber == null)
+                {
+                    break;
+                }
+                fio = Console.ReadLine();
+                phone.Add(phoneNumber, fio);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        foreach (KeyValuePair<string, string> pair in phone)
+        {
+            Console.WriteLine("Номер телефона: " + pair.Key + "ФИО" + pair.Value);
+        }
+        #endregion
+    }
+}
